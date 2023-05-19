b4d2027e229f467d2ec6ce435b0addab4d217e5e
sbx_user1051 <sbx_user1051@169.254.73.133>
2023-05-19 11:15:31 +0000

    Fri May 19 11:15:31 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,11 +2,28 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CheckoutSolution {
+    private skuObject objectA = new skuObject("A", 50);
+    private skuObject objectB = new skuObject("B", 30);
+    private skuObject objectC = new skuObject("C", 20);
+    private skuObject objectD = new skuObject("D", 15);
+
+    private Map<String, Integer> mapOfSkuToPrice = new HashMap<>();
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+        mapOfSkuToPrice.put("A", 50);
+        mapOfSkuToPrice.put("B", 30);
+        mapOfSkuToPrice.put("C", 20);
+        mapOfSkuToPrice.put("D", 15);
+
+        
+
+
     }
 }
+
 
 
 
--- src/main/java/befaster/solutions/CHK/skuObject.java
+++ src/main/java/befaster/solutions/CHK/skuObject.java
@@ -1,6 +1,16 @@
 package befaster.solutions.CHK;
 
 public class skuObject {
+    private String name;
+    private int price;
 
-    
+    public skuObject(String name, int price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
 }
+
