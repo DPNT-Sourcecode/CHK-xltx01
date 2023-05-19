8e2149a3db1be1ba1640162cd8f68a1025fbd564
sbx_user1051 <sbx_user1051@169.254.73.133>
2023-05-19 15:08:31 +0000

    Fri May 19 15:08:31 UTC 2023

---
+++ src/main/java/befaster/solutions/CHK/skuF.java
@@ -0,0 +1,13 @@
+package befaster.solutions.CHK;
+
+public class skuE extends skuObject {
+    public skuE(String name, int price) {
+        super(name, price);
+    }
+
+    public int calculateBasketSum(int totalSkuEInBasket){
+        Integer sum = 0;
+        sum = sum + (totalSkuEInBasket * 40);
+        return sum;
+    }
+}
