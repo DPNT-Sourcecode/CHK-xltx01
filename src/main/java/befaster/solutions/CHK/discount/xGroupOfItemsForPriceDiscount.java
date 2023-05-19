480eca1a53a525a10d3094271c309234a1a19793
sbx_user1051 <sbx_user1051@169.254.73.133>
2023-05-19 21:34:56 +0000

    Fri May 19 21:34:56 UTC 2023

---
+++ src/main/java/befaster/solutions/CHK/discount/xGroupOfItemsForPriceDiscount.java
@@ -0,0 +1,24 @@
+package befaster.solutions.CHK.discount;
+
+import befaster.solutions.CHK.skuObject;
+
+public class xItemForPriceDiscount extends discount implements DiscountCalculator{
+
+    private skuObject discountItem;
+
+    private int discountPrice;
+
+    public xItemForPriceDiscount(skuObject discountItem, int itemCount, int discountPrice) {
+        super(itemCount);
+        this.discountItem = discountItem;
+        this.discountPrice = discountPrice;
+    }
+
+    public int calculateDiscount(int count, int countOfDependentItem) {
+        int quotient = count / getItemCount() ;
+        if (quotient == 0) return count;
+        int reminder = count % getItemCount();
+        discountItem.setTotal(discountItem.getTotal() + (quotient * discountPrice));
+        return reminder;
+    }
+}
